c53bbf92b1192a3e100d7b44e14f9bf9780b7808
Allen Kovacevic <akovacevic4@etf.unsa.ba>
2019-10-15 17:21:50 +0200

    Zavrsen zadatak

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    public final int max=50;
+    private static int brElemenata=0;
+    private Artikl[] niz=null;
+
+    public Korpa(){
+        niz=new Artikl[max];
+    }
+    public boolean dodajArtikl(Artikl artikal){
+        if(brElemenata>max-1) return false;
+        this.niz[brElemenata]=new Artikl(artikal.getNaziv(), artikal.getCijena(), artikal.getKod());
+        brElemenata++;
+        return true;
+    }
+    public Artikl[] getArtikli(){
+        return this.niz;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl izbacen=null;
+        for(int i=0; i<this.brElemenata; i++){
+            if(this.niz[i].getKod().equals(kod)){
+                izbacen=this.niz[i];
+                this.niz[i]=null;
+                for(int j=i; j<brElemenata-1; j++){
+                    this.niz[j]=this.niz[j+1];
+                }
+                this.brElemenata--;
+                this.niz[brElemenata]=null;
+                break;
+            }
+        }
+        return izbacen;
+    }
+    public int dajUkupnuCijenuArtikala(){
+        int suma=0;
+        for(int i=0; i<brElemenata; i++){
+            suma=suma+this.niz[i].getCijena();
+        }
+        return suma;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    public final int max=1000;
+    private static int brElemenata=0;
+    private Artikl[] niz=null;
+
+    public Supermarket(){
+        niz=new Artikl[max];
+    }
+    public boolean dodajArtikl(Artikl temp){
+        if(brElemenata>max-1) return false;
+        this.niz[brElemenata]=new Artikl(temp.getNaziv(), temp.getCijena(), temp.getKod());
+        brElemenata++;
+        return true;
+    }
+    public Artikl[] getArtikli(){
+        return this.niz;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl izbacen=null;
+        for(int i=0; i<this.brElemenata; i++){
+            if(this.niz[i].getKod().equals(kod)){
+                izbacen=this.niz[i];
+                for(int j=i; j<this.brElemenata-1; j++){
+                    this.niz[j]=this.niz[j+1];
+                }
+                this.brElemenata--;
+                this.niz[brElemenata]=null;
+                break;
+            }
+        }
+        return izbacen;
+    }
 }
